981db4129e44074c935778db04204d9b9513c50f
John Turner Dick <JD@Johns-MacBook-Pro.local>
2023-12-09 07:24:47 +0000

    refactor code
    this is the refactoring of my very inefficient solution

--- starter/index.js
+++ starter/index.js
@@ -87,7 +87,6 @@ var finances = [
   ["Feb-2017", 671099],
 ];
 
-/*
 // Calculate number of months
 
 console.log(`******* number of months *******`);
@@ -98,128 +97,12 @@ console.log(numberMonths);
 // Net total profit and loss
 
 console.log(`******* net profit & loss *******`);
-
-let financeNumbers = [];
+var profit = 0;
+// this sets the revenue number
+var netProfit = 0;
+// this sets the revenue counter
 for (i = 0; i < finances.length; i++) {
-  financeNumbers[i] = finances[i][1];
+  profit = finances[i][1];
+  netProfit += profit;
 }
-console.log(`No of monthly amounts identified: ${financeNumbers.length}`);
-
-console.log(`********* here is the array of numbers ********`);
-
-console.log(financeNumbers);
-
-// Calculate the net position //
-
-console.log(`********* This is the net position ********`);
-
-let totalProfitNumbers = 0;
-let totalLoss = 0;
-
-for (i = 0; i < financeNumbers.length; i++) {
-  const calcNumber = financeNumbers[i];
-  if (calcNumber > 1) {
-    totalProfitNumbers += calcNumber;
-  } else {
-    totalLoss += calcNumber * -1;
-  }
-}
-console.log(`********* Here are the totals ********`);
-
-console.log(`********* Profit numbers  ********`);
-console.log(totalProfitNumbers);
-
-console.log(`********* Loss numbers ********`);
-console.log(totalLoss);
-
-let netPosition = totalProfitNumbers - totalLoss;
-console.log(`********* net position ********`);
-console.log(netPosition);
-
-console.log(`********* Monthly Change Numbers ********`);
-
-let monthlyChange = [];
-for (i = 0; i < financeNumbers.length - 1; i++) {
-  monthlyChange[i] = financeNumbers[i + 1] - financeNumbers[i];
-}
-console.log(monthlyChange);
-
-let totalChange = 0;
-for (i = 0; i < monthlyChange.length; i++) {
-  totalChange += monthlyChange[i];
-}
-console.log(totalChange);
-
-let averageChange = totalChange / (numberMonths - 1);
-
-console.log(averageChange);
-
-console.log(`***** Calculate biggest monthly change (+ or -v) ****`)
-
-*/
-
-// Create monthly change array for looping comparison to work out largest change
-// This tells me the changes each month
-
-var specifiedMonthChange = [];
-
-for (i = 1; i <= finances.length - 1; i++) {
-  specifiedMonthChange[i - 1] = [
-    finances[i][i - i],
-    finances[i][1] - finances[i - 1][1],
-  ];
-}
-console.log(specifiedMonthChange);
-
-// Now we need to loop through to detemine the largest
-// Find the biggest change
-
-var maxChangePostiveMonth = 0;
-var maxMonth;
-
-for (i = 0; i <= specifiedMonthChange.length - 1; i++) {
-  if (
-    specifiedMonthChange[i][1] > 1 &&
-    specifiedMonthChange[i][1] > maxChangePostiveMonth
-  ) {
-    maxChangePostiveMonth = specifiedMonthChange[i][1];
-    maxMonth = specifiedMonthChange[i][0];
-  }
-
-  /*
-  if (specifiedMonthChange[i][1] > specifiedMonthChange[i - 1][1]) {
-    maxChangePostiveAmt = specifiedMonthChange[i][1];
-    maxChangePostiveMonth = specifiedMonthChange[i][0];
-  }*/
-}
-
-console.log(maxChangePostiveMonth, maxMonth);
-
-/*
-  ["Jan-2010", 867884],
-  ["Feb-2010", 984655],
-  ["Mar-2010", 322013],
-  ["Apr-2010", -69417],
-
-*/
-
-var maxChangeLossMonth = 0;
-var maxlossMonth;
-
-for (i = 0; i <= 83; i++) {
-  if (
-    specifiedMonthChange[i][1] < 1 &&
-    specifiedMonthChange[i][1] < maxChangePostiveMonth
-  ) {
-    maxChangePostiveMonth = specifiedMonthChange[i][1];
-    maxMonth = specifiedMonthChange[i][0];
-  }
-
-  /*
-  if (specifiedMonthChange[i][1] > specifiedMonthChange[i - 1][1]) {
-    maxChangePostiveAmt = specifiedMonthChange[i][1];
-    maxChangePostiveMonth = specifiedMonthChange[i][0];
-  }*/
-}
-
-console.log(maxChangePostiveMonth, maxMonth);
+console.log(`Net total amount is: $${netProfit}`);
